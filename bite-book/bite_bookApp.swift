2ca12819cfc2b8b3f92245bb0db79f1d6f7d2fb0
Sanskar Mani <sanskarmani@gmail.com>
2026-02-04 21:36:00 +0000

    Initial Commit

---
+++ bite-book/bite_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  bite_bookApp.swift
+//  bite-book
+//
+//  Created by Sanskar Mani on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct bite_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
